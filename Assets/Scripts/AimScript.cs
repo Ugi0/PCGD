57e864edd04477cceff67b2cf533cfaaca9f0db3
jaakkou98 <jaakkou98@gmail.com>
2025-01-28 16:45:32 +0200

    unity 6 project from 2d mobile temp
    has working throwing mechanism

---
+++ Assets/Scripts/AimScript.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+public class AimScript : MonoBehaviour
+{
+    public GameObject throwingHand;
+    public GameObject throwableObject;
+    public GameObject aimingHand;
+    private bool aiming = false;
+    private bool powering = false;
+    public float aimingSpeed;
+    public float poweringSpeed;
+    public float maxZRotation;
+    public float minZRotation;
+    public float maxXPosition;
+    public float minXPosition;
+    private float rotation = 0;
+    private float position = 0;
+    public float throwPower;
+    private float throwPowerScaler;
+    void OnMouseUp()
+    {
+        if(powering)
+        {   
+            powering = false;
+            ThrowObject();
+        }
+        else if(!aiming)
+        {
+            aiming = true;
+        }
+        else
+        {
+            powering = true;
+            aiming = false;
+        }
+    }
+
+    void Update()
+    {
+        if(aiming)
+        {
+            transform.Rotate(0.0f,0.0f,aimingSpeed,Space.Self);
+            rotation = transform.localRotation.z;
+            if(rotation > maxZRotation || rotation < minZRotation)
+            {
+                aimingSpeed = -aimingSpeed;
+            }
+        }
+        if(powering)
+        {
+            throwingHand.transform.Translate(poweringSpeed,0,0);
+            position = throwingHand.transform.localPosition.x;
+            if(position > maxXPosition || position < minXPosition)
+            {
+                poweringSpeed = -poweringSpeed;
+            }
+        }
+    }
+
+    private void ThrowObject()
+    {
+        throwableObject.transform.parent = null;
+        throwableObject.GetComponent<CircleCollider2D>().enabled = true;
+        Rigidbody2D rigidbody2D = throwableObject.GetComponent<Rigidbody2D>();
+        rigidbody2D.constraints = RigidbodyConstraints2D.None;
+        throwPowerScaler = 100-(position-minXPosition)*(100 / (maxXPosition - minXPosition));
+        rigidbody2D.AddForce(transform.right*throwPower*throwPowerScaler);
+    }
+}
