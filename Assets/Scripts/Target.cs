d4176a6189d8745571131ac086e1b9d062381e3d
Ugi <90718205+Ugi0@users.noreply.github.com>
2025-02-05 21:09:15 +0200

    Add a target that will choose a random position based on the parameters in the edtiro

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -2,6 +2,21 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    [SerializeField] GameObject spawnArea;
+    [SerializeField] float distanceFromPlayer;
+    [SerializeField] float heightOfTarget;
+
+    void Start() {
+        float colliderWidth = spawnArea.GetComponent<BoxCollider2D>().bounds.size.x;
+        float colliderHeight = spawnArea.GetComponent<BoxCollider2D>().bounds.size.y;
+        Vector3 rescale = gameObject.transform.localScale;
+        rescale.y = heightOfTarget;
+        gameObject.transform.localScale = rescale;
+        gameObject.transform.position = new Vector2(
+            Random.Range(spawnArea.transform.position.x - colliderWidth, spawnArea.transform.position.x) + distanceFromPlayer, 
+            Random.Range(spawnArea.transform.position.y - colliderHeight, spawnArea.transform.position.y)
+        );
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("ThrowableObject"))
